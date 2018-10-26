9f778b65b3f155ae0fa0e8f8fa452c18f553992d
jinzhiyong1990 <jinzhiyong1990@gmail.com@server.com>
2018-10-25 17:42:17 -0700

    Initial commit

---
+++ app/src/main/java/com/example/zhiyongjin/mutao/fragment/MainFragment.java
@@ -0,0 +1,26 @@
+package com.example.zhiyongjin.mutao.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.zhiyongjin.mutao.R;
+
+import java.util.zip.Inflater;
+
+/*
+* 主界面视图
+* */
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+    }
+}
